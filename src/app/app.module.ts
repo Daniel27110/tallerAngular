28a207e07c13241827d6af6f2d52fe644eea55ea
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-04-21 08:59:07 -0500

    Serie module Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SerieModule } from './serie/serie.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SerieModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/serie/serie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'series-list',
+  templateUrl: './serie.component.html',
+  styleUrls: ['./serie.component.css']
+})
+export class SerieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
