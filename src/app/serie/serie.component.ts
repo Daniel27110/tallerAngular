33d61ae5eb1a57e096b8e47721d9ca703ffce3a4
Daniel27110 <d.vargasu@uniandes.edu.co>
2023-04-22 17:39:36 -0500

    Promedio DONE

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -11,14 +11,33 @@ export class SerieComponent implements OnInit {
 
   constructor(private serieRequestService: SerieRequestService) { }
   series: Array<Serie> = [];
+  avgSeasons: number = 0;
 
   getSeries(): void {
     this.serieRequestService.getSeries().subscribe(series => this.series = series);
   }
 
+  getAvgSeasons(): void {
+    // for (let serie of this.series) {
+    //   this.avgSeasons += serie.seasons;
+    // }
+    // this.avgSeasons /= this.series.length;
+
+    // This won't work because the series array is empty when this method is called
+
+    this.serieRequestService.getSeries().subscribe(series => {
+      for (let serie of series) {
+        this.avgSeasons += serie.seasons;
+      }
+      this.avgSeasons /= series.length;
+    });
+
+  }
+
 
   ngOnInit() {
     this.getSeries();
+    this.getAvgSeasons();
   }
 
 }
